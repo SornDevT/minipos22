42320090fd7712b64b897376117d565b852ff86a
SornDev <sorndev.teach@gmail.com>
2025-01-22 22:58:12 +0700

    install template, vue-router VDO 02

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/core.css') }}" class="template-customizer-core-css">
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="{{ url('assets/css/demo.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/font_lao.css') }}">
+    
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     @vite('resources/css/app.css')
 </head>
 <body>
 
-    <h1>Hello Laravel V11</h1> <h2> my app v1 </h2>
-
-    <hr>
-
     <div id="app-vue"></div>
+
+  <script src="{{ url('assets/vendor/libs/jquery/jquery.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/popper/popper.js') }}"></script>
+  <script src="{{ url('assets/vendor/js/bootstrap.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+  <script src="{{ url('assets/vendor/js/menu.js') }}"></script>
     
     @vite('resources/js/app.js')
 </body>
